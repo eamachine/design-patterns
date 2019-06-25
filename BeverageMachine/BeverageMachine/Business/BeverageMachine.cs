f30f5525625515f4935eb1473703f08387c67ad3
eamachine <eamachine@gmail.com>
2019-06-25 15:20:45 -0500

    BeverageMachine

---
+++ BeverageMachine/BeverageMachine/Business/BeverageMachine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeverageMachine.Business
+{
+    public class BeverageMachine
+    {
+        public void MakeCoffe()
+        {
+
+        }
+
+        public void MakeChocolate()
+        {
+
+        }
+    }
+}
